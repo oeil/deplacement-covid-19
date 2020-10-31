5bbbb489f972a3f3755e07fad85c858832ce189d
oeil <perso.feyl@gmail.com>
2020-10-31 15:18:11 +0100

    fixes fields

--- script/generate.js
+++ script/generate.js
@@ -5,20 +5,20 @@ window.addEventListener("DOMContentLoaded", (event) => {
     document.getElementById("field-firstname").value = urlParams.get("f"); //firstname
     document.getElementById("field-lastname").value = urlParams.get("l"); //lastname
     document.getElementById("field-birthday").value = urlParams.get("b"); //birthday
-    document.getElementById("field-lieunaissance").value = urlParams.get("p"); //place of born
+    document.getElementById("field-placeofbirth").value = urlParams.get("p"); //place of born
     document.getElementById("field-address").value = urlParams.get("a"); //address
-    document.getElementById("field-town").value = urlParams.get("c"); //city
+    document.getElementById("field-city").value = urlParams.get("c"); //city
     document.getElementById("field-zipcode").value = urlParams.get("z"); //zipcode
     const reason = urlParams.get("r"); //reason
-    document.getElementById("checkbox-travail").checked = (reason==='travail');
-    document.getElementById("checkbox-achats").checked = (reason==='achats');
-    document.getElementById("checkbox-enfants").checked = (reason==='enfants');
-    document.getElementById("checkbox-sport_animaux").checked = (reason==='sport_animaux');
-    document.getElementById("checkbox-sante").checked = (reason==='sante');
-    document.getElementById("checkbox-famille").checked = (reason==='famille');
-    document.getElementById("checkbox-handicap").checked = (reason==='handicap');
-    document.getElementById("checkbox-convocation").checked = (reason==='convocation');
-    document.getElementById("checkbox-missions").checked = (reason==='missions');
+    document.getElementById("radio-travail").checked = (reason==='travail');
+    document.getElementById("radio-achats").checked = (reason==='achats');
+    document.getElementById("radio-enfants").checked = (reason==='enfants');
+    document.getElementById("radio-sport_animaux").checked = (reason==='sport_animaux');
+    document.getElementById("radio-sante").checked = (reason==='sante');
+    document.getElementById("radio-famille").checked = (reason==='famille');
+    document.getElementById("radio-handicap").checked = (reason==='handicap');
+    document.getElementById("radio-convocation").checked = (reason==='convocation');
+    document.getElementById("radio-missions").checked = (reason==='missions');
 
     var now = new Date()
     document.getElementById('field-datesortie').value = now.toJSON().slice(0,10);
